7c4097ee9c112e0046af0a5f636b2b7fd5326555
Leandro Cabeda Rigo <leandro.cabeda@hotmail.com>
2018-08-21 00:43:39 -0300

    geração de horarios concluidas o problema

--- src/pages/bus-linha/bus-linha.ts
+++ src/pages/bus-linha/bus-linha.ts
@@ -14,48 +14,18 @@ import { bind2 } from '@angular/core/src/render3/instructions';
 })
 export class BusLinhaPage {
   public bus: Onibus;
-
-  public te:Bairros[]=[];
-  public ho:string[]=[];
-  public bar:Bairros[];
-  public bar2: Bairros[];
-  public bar3: Bairros[];
+  public bus2:Bairros[];
+  public bus3: Bairros[];
+  public bus4: Bairros[];
 
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.bus = navParams.get("bus");
     console.log("Valor que chegou no bus de linhas: "+this.bus);
-    this.bar=this.bus.sabados.bairros;
-    this.te=this.bus.sabados.bairros;
-    this.bus.sabados.bairros.forEach(f=>{ this.ho.push(f.horarios.join(" "))});
-
-    /*for (const b of this.bar) {
-
-      for (const b2 of this.te) {
-
-        if(b2.nome===b.nome)
-        {
-          this.ho.push(b.horarios.join(" "));
-        }
-
-      }
-      
-    }*/
-
-
-    /*this.te=this.bar.filter(e=> {
-      e.horarios.filter(f=>{
-          this.ho.push(f);
-      })
-      return e;
-    });*/
-    
-    
-    console.log("Valor que chegou no bar de bairros: " + this.bar);
-    this.bar2 = this.bus.dias.bairros;
-    this.bar3= this.bus.domingos_feriados.bairros;
-  
+    this.bus2=this.bus.sabados.bairros;
+    this.bus3 = this.bus.dias.bairros;
+    this.bus4 = this.bus.domingos_feriados.bairros;
    
     
 
