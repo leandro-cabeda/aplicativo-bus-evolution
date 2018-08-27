95ab172e3197363d25dc2c94b472209045d2b849
Leandro Cabeda Rigo <leandro.cabeda@hotmail.com>
2018-08-26 22:48:14 -0300

    editando uns dados

--- src/pages/google-map/google-map.ts
+++ src/pages/google-map/google-map.ts
@@ -1,6 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component} from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, Platform } from 'ionic-angular';
 import { Onibus } from '../../models/Onibus';
+import { Geolocation } from '@ionic-native/geolocation';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
+
+declare var google:any;
 
 @IonicPage()
 @Component({
@@ -9,8 +21,24 @@ import { Onibus } from '../../models/Onibus';
 })
 export class GoogleMapPage{
   public bus:Onibus;
+  public map:any;
+  public marker:any;
+  public lugar=[
+  {
+    "nome":"lugar 1",
+      "lat": -28.264988,
+      "lng": - 52.418995
+  },
+  {
+    "nome": "lugar 2",
+    "lat": -28.265800,
+    "lng": - 52.419381
+  }
+];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private platform:Platform,
+    private alert: AlertController,
+    private geolocation: Geolocation) {
     this.bus = this.navParams.get("bus");
   }
 
@@ -23,12 +51,93 @@ export class GoogleMapPage{
   }*/
 
   ionViewDidEnter() {
+    this.platform.ready().then(()=>{
     this.carregarMapa();
+    });
   }
 
   public carregarMapa()
   {
+    this.geolocation.getCurrentPosition().then(result=>{
+      this.loadMap(result.coords.latitude,result.coords.longitude);
+    });
+  }
 
+  public loadMap(lat,lng)
+  {
+    let latlng= new google.maps.LatLng(lat,lng);
+
+    let mapOptions={
+      // minha localização de latitude e longitude
+      center:latlng,
+      zoom:14,
+      // esse tipo será tipo de mapa terrestre
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI:true
+    }
+
+    let element=document.getElementById("map");
+
+    this.map=new google.maps.Map(element,mapOptions);
+
+    // Faz a marcação onde gente se encontra
+    let marker= new google.maps.Marker({
+      // position recebe a longitude e latitude de onde se encontra
+      position: latlng,
+      title: "Minha localização",
+      icon:"http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+    });
+
+    let content='<div id="myId" class="item item-thumbnail-left item-text-wrap">'+
+      '<ion-item>'+
+        '<ion-row>'+
+          '<h6>'+marker.title+'</h6>'+
+        '</ion-row>'+
+      '</ion-item>'+
+    '</div>';
+
+    marker.setMap(this.map);
+
+    this.addinfowindow(marker,content);
+
+    this.loadPoints();
+
+  }
+
+  public addinfowindow(marker,content)
+  {
+    let infowindow= new google.maps.InfoWindow({
+      content:content
+    });
+
+    google.maps.event.addListener(marker,"click",()=>{
+      // Isso faz quando clicar no marcador faz apareçer a descrição que foi enviado
+      infowindow.open(this.map,marker);
+    });
+
+  }
+
+  public loadPoints() {
+    this.marker = [];
+
+    for (const key of Object.keys(this.lugar)) {
+      let latlng = new google.maps.LatLng(this.lugar[key].lat, this.lugar[key].lng);
+      let marker = new google.maps.Marker({
+        position: latlng,
+        title: this.lugar[key].nome
+      });
+
+      let content = '<div id="myId" class="item item-thumbnail-left item-text-wrap">' +
+        '<ion-item>' +
+        '<ion-row>' +
+        '<h6>' + this.lugar[key].nome + '</h6>' +
+        '</ion-row>' +
+        '</ion-item>' +
+        '</div>';
+
+      this.addinfowindow(marker, content);
+      marker.setMap(this.map);
+    }
   }
 
 }
