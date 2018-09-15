6340e5810a7d9ea4bd6306ad8bb3826ba18b196d
Leandro Cabeda Rigo <leandro.cabeda@hotmail.com>
2018-09-15 19:17:59 -0300

    teste finalizado e concluido com google maps

--- src/pages/google-map/google-map.ts
+++ src/pages/google-map/google-map.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Onibus } from '../../models/Onibus';
 import { Geolocation } from '@ionic-native/geolocation';
@@ -18,19 +18,50 @@ import { HttpServiceProvider } from '../../providers/http-service/http-service';
 import { HttpErrorResponse } from '@angular/common/http';
 
 // Essa variavel é do namespace da própria google api, tem que ser declarada
-declare var google:any;
+declare var google: any;
 
 @IonicPage()
 @Component({
   selector: 'page-google-map',
   templateUrl: 'google-map.html',
 })
-export class GoogleMapPage{
+export class GoogleMapPage {
   private alert: AlertController;
-  public bus:Onibus[];
-  public map:any;
-  public marker:any;
-  public lugar=[
+  //public bus:Onibus[];
+  public map: any;
+  public marker: any;
+  public bus = {
+    "linha": "VERA CRUZ SAO CRISTOVAO",
+    "rotas": [
+      [-28.280337621823495, -52.371343374252326],
+      [-28.27937389676426, -52.37116634845734],
+      [-28.279000687168416, -52.371912002563484],
+      [-28.277777117422552, -52.37171351909638],
+      [-28.27688423333127, -52.37366616725922],
+      [-28.280550207059587, -52.37700283527375],
+      [-28.27873140854655, -52.379620671272285],
+      [-28.27714879236088, -52.38054871559143],
+      [-28.27563701762648, -52.38150358200074],
+      [-28.273444906149916, -52.38376736640931],
+      [-28.27269371964141, -52.385355234146125],
+      [-28.268611707908512, -52.38930344581605],
+      [-28.268125068761442, -52.39146530628205],
+      [-28.266542295000786, -52.395091652870185],
+      [-28.26515321447945, -52.40153431892396],
+      [-28.265767435990718, -52.40438282489777],
+      [-28.265923353195554, -52.40687727928162],
+      [-28.26170878364624, -52.40936636924744],
+      [-28.262880564105377, -52.41198420524597],
+      [-28.256563191965796, -52.41612017154694],
+      [-28.25358628997839, -52.41812109947205],
+      [-28.253175187457575, -52.418560981750495],
+      [-28.250552599584, -52.4228847026825],
+      [-28.24940903656801, -52.42370009422303],
+      [-28.251450429721253, -52.427809238433845],
+      [-28.248133146001525, -52.430899143219]
+    ],
+  };
+  /*public lugar=[
   {
     "nome":"lugar 1",
       "lat": -28.264988,
@@ -41,7 +72,7 @@ export class GoogleMapPage{
     "lat": -28.265800,
     "lng": - 52.419381
   }
-];
+];*/
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private geolocation: Geolocation, private apibus: HttpServiceProvider) {
@@ -52,7 +83,7 @@ export class GoogleMapPage{
     console.log("Entrou no DidEnter");
     try {
       this.carregarMapa();
-      this.buscarLinhas();
+      //this.buscarLinhas();
 
     } catch (error) {
       this.alert.create({
@@ -70,7 +101,7 @@ export class GoogleMapPage{
 
   }
 
-  public buscarLinhas()
+  /*public buscarLinhas()
   {
     this.apibus.listaTodos()
       .subscribe((onibus) => {
@@ -92,22 +123,21 @@ export class GoogleMapPage{
           .present();
       }
       );
-  }
+  }*/
 
-  public carregarMapa()
-  {
+  public carregarMapa() {
     console.log("Entrou na função carregarmapa");
-    this.geolocation.getCurrentPosition().then(result=>{
+    this.geolocation.getCurrentPosition().then(result => {
       //this.loadMap(result.coords.latitude,result.coords.longitude);
       this.carregaRota(result.coords.latitude, result.coords.longitude);
 
-    }).catch(err=>{
+    }).catch(err => {
       this.alert.create({
         title: "Alerta!!",
         subTitle: "GPS está fora",
-        buttons:[{
+        buttons: [{
           text: "Confirmar",
-          handler:()=>{
+          handler: () => {
             this.navCtrl.setRoot(HomePage);
           }
         }]
@@ -115,47 +145,47 @@ export class GoogleMapPage{
     });
   }
 
- /* public loadMap(lat,lng)
-  {
-    console.log("Entrou na função loadmap");
-    let latlng= new google.maps.LatLng(lat,lng);
+  /* public loadMap(lat,lng)
+   {
+     console.log("Entrou na função loadmap");
+     let latlng= new google.maps.LatLng(lat,lng);
 
-    let mapOptions={
-      // minha localização de latitude e longitude
-      center:latlng,
-      zoom:16,
-      // esse tipo será tipo de mapa terrestre
-      mapTypeId:google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI:true
-    }
+     let mapOptions={
+       // minha localização de latitude e longitude
+       center:latlng,
+       zoom:16,
+       // esse tipo será tipo de mapa terrestre
+       mapTypeId:google.maps.MapTypeId.ROADMAP,
+       disableDefaultUI:true
+     }
 
-    let element=document.getElementById("map");
+     let element=document.getElementById("map");
 
-    this.map=new google.maps.Map(element,mapOptions);
+     this.map=new google.maps.Map(element,mapOptions);
 
-    // Faz a marcação onde gente se encontra
-    let marker= new google.maps.Marker({
-      // position recebe a longitude e latitude de onde se encontra
-      position: latlng,
-      title: "Minha localização",
-      icon:"http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-    });
+     // Faz a marcação onde gente se encontra
+     let marker= new google.maps.Marker({
+       // position recebe a longitude e latitude de onde se encontra
+       position: latlng,
+       title: "Minha localização",
+       icon:"http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+     });
 
-    let content='<div id="myId" class="item item-thumbnail-left item-text-wrap">'+
-      '<ion-item>'+
-        '<ion-row>'+
-          '<h6>'+marker.title+'</h6>'+
-        '</ion-row>'+
-      '</ion-item>'+
-    '</div>';
+     let content='<div id="myId" class="item item-thumbnail-left item-text-wrap">'+
+       '<ion-item>'+
+         '<ion-row>'+
+           '<h6>'+marker.title+'</h6>'+
+         '</ion-row>'+
+       '</ion-item>'+
+     '</div>';
 
-    marker.setMap(this.map);
+     marker.setMap(this.map);
 
-    this.addinfowindow(marker,content);
+     this.addinfowindow(marker,content);
 
-    this.loadPoints();
+     this.loadPoints();
 
-  }*/
+   }*/
 
   /*public loadPoints() {
     console.log("Entrou na função loadPoints");
@@ -180,23 +210,11 @@ export class GoogleMapPage{
       marker.setMap(this.map);
     }
   }*/
-  public addinfowindow(marker, content) {
-    //console.log("Entrou na função addinfowindow");
-    let infowindow = new google.maps.InfoWindow({
-      content: content
-    });
 
-    google.maps.event.addListener(marker, "click", () => {
-      // Isso faz quando clicar no marcador faz apareçer a descrição que foi enviado
-      infowindow.open(this.map, marker);
-    });
-
-  }
-
-  public carregaRota(lat,lng)
-  {
+  public carregaRota(lat, lng) {
     //this.loadPoints();
-
+    var tam = this.bus.rotas.length;
+    var bu = this.bus.rotas;
     var directionService = new google.maps.DirectionsService();
     var directionDisplay = new google.maps.DirectionsRenderer();
     //var bounds = new google.maps.LatLngBounds();
@@ -207,21 +225,32 @@ export class GoogleMapPage{
       center: latlng,
       zoom: 16,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
+      disableDefaultUI: true // desativar as configurações padrões da interface do usuário ao api
     }
 
     let element = document.getElementById("map");
 
     var map = new google.maps.Map(element, mapOptions);
 
+    var iconmod = function () {
+      return {
+        fillColor: 'black',
+        fillOpacity: .5,
+        strokeColor: 'white',
+        strokeWeight: .8,
+        draggable: true,
+      }
+    }
+
     var marker = new google.maps.Marker({
       // position recebe a longitude e latitude de onde se encontra
       position: latlng,
       title: "Minha localização",
-      icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+      map: map,
+      icon: iconmod //"http://maps.google.com/mapfiles/ms/icons/green-dot.png"
     });
-     marker.setMap(map);
 
+    marker.setMap(map);
 
     var content = '<div id="myId" class="item item-thumbnail-left item-text-wrap">' +
       '<ion-item>' +
@@ -231,8 +260,9 @@ export class GoogleMapPage{
       '</ion-item>' +
       '</div>';
 
+
     directionDisplay.setMap(map);
-    var geocoder = new google.maps.Geocoder();
+    //var geocoder = new google.maps.Geocoder();
     //let service = new google.maps.DistanceMatrixService;
 
 
@@ -255,24 +285,24 @@ export class GoogleMapPage{
 
     //var destination = new google.maps.LatLng(this.lugar[0].lat,this.lugar[0].lng);
 
-    var lt =""+this.bus[0].rotas[0][0];
-    var lg =""+this.bus[0].rotas[0][1];
-    console.log("valor lt: "+lt);
+    var lt = "" + bu[0][0];
+    var lg = "" + bu[0][1];
+    console.log("valor lt: " + lt);
     console.log("valor lg: " + lg);
-      var dest = new google.maps.LatLng(
-        parseFloat(lt), parseFloat(lg)
-        );
+    var dest = new google.maps.LatLng(
+      parseFloat(lt), parseFloat(lg)
+    );
 
-    var lt2 = "" + this.bus[0].rotas[22][0];
-    var lg2 = "" + this.bus[0].rotas[22][1];
+
+    var lt2 = "" + bu[16][0];
+    var lg2 = "" + bu[16][1];
     console.log("valor lt2: " + lt2);
     console.log("valor lg2: " + lg2);
     var dest2 = new google.maps.LatLng(
       parseFloat(lt2), parseFloat(lg2)
     );
 
-
-    const request={
+    const request = {
       //oirigin pode ser uma coordenada(LatLng),uma string ou um lugar
       // obrigatório por
       origin: dest,
@@ -280,7 +310,7 @@ export class GoogleMapPage{
       // obrigatório por
       destination: dest2,
       // TravelModel Especifica o tipo de transporte a ser calculado
-      travelMode:"TRANSIT",
+      travelMode: "DRIVING",
       /*unitSystem:google.maps.UnitSystem.METRIC
       É utilizado para mostrar o calculado da rota mostrado em quilometros*/
       unitSystem: google.maps.UnitSystem.METRIC,
@@ -298,20 +328,19 @@ export class GoogleMapPage{
     }
 
 
-    var msg="";
-    var resultlist=[];
-    var markerArray=[];
+    var msg = "";
+    //var resultlist = [];
+    //var markerArray = [];
     var destinationIcon = 'https://chart.googleapis.com/chart?' +
       'chst=d_map_pin_letter&chld=D|FF0000|000000';
 
-    directionService.route(request,(result,status)=>{
-      resultlist.push(result);
-      switch(status)
-      {
+    directionService.route(request, (result, status) => {
+      //resultlist.push(result);
+      switch (status) {
         case "OK":
           directionDisplay.setDirections(result);
           //map.fitBounds(bounds.extend(result));
-        break;
+          break;
 
         case "NOT_FOUND":
           msg = "Local ou destino de algum ponto não pode ser geocodificado!!";
@@ -323,57 +352,74 @@ export class GoogleMapPage{
           break;
 
         case "INVALID_REQUEST":
-          msg="Inválido solicitação, está faltando origem ou destino";
+          msg = "Inválido solicitação, está faltando origem ou destino";
 
           break;
 
         case "OVER_QUERY_LIMIT":
-          msg="A página enviou muitas solicitações dentro do seu limite permitido. Pedimos desculpa!";
+          msg = "A página enviou muitas solicitações dentro do seu limite permitido. Pedimos desculpa!";
           break;
         case "REQUEST_DENIED":
-          msg="Pedimos desculpa, mas a página da web não tem permissão para usar esse percurso de rota";
+          msg = "Pedimos desculpa, mas a página da web não tem permissão para usar esse percurso de rota";
           break;
 
         case "UNKNOWN_ERROR":
-          msg="A Rota não pode ser processada por motivo de erro no servidor. Pedimos desculpa";
+          msg = "A Rota não pode ser processada por motivo de erro no servidor. Pedimos desculpa";
           break;
 
         default:
-          msg="Foram indicados muitos pontos de rota ou a rota solicitada é muito longa e não pode ser processada";
-        break;
+          msg = "Foram indicados muitos pontos de rota ou a rota solicitada é muito longa e não pode ser processada";
+          break;
 
       }
 
-      if(status!="OK")
-      {
+      if (status != "OK") {
         this.alert.create({
-          title:"Alerta",
-          message:msg,
-          buttons:[{
-            text:"Confirmar",
-            handler:()=>{
+          title: "Alerta",
+          message: msg,
+          buttons: [{
+            text: "Confirmar",
+            handler: () => {
               this.navCtrl.setRoot(HomePage);
             }
           }]
         });
       }
-      else if(status=="OK")
-      {
-        for(let i=0;i<resultlist.length;i++)
-        {
-          geocoder.geocode({"address":resultlist[i]});
+      /*else if (status == "OK") {
+        for (let i = 0; i < resultlist.length; i++) {
+          geocoder.geocode({ "address": resultlist[i] });
           markerArray.push(new google.maps.Marker({
-            map:map,
+            map: map,
             position: result,
             icon: destinationIcon
           }));
         }
-      }
+      }*/
+      var marker2 = new google.maps.Marker({
+        map: map,
+        position: result,
+        icon: destinationIcon
+      });
+
+      marker2.setMap(map);
 
     });
 
-
     this.addinfowindow(marker, content);
+
+  }
+
+  public addinfowindow(marker, content) {
+    //console.log("Entrou na função addinfowindow");
+    let infowindow = new google.maps.InfoWindow({
+      content: content
+    });
+    infowindow.open(this.map, marker); // já deixa mostrando o conteudo da descrição
+
+    google.maps.event.addListener(marker, "click", () => {
+      // Isso faz quando clicar no marcador faz apareçer a descrição que foi enviado
+      infowindow.open(this.map, marker);
+    });
 
   }
 
